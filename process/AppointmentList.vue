1735e6fba08ee9a0e90b196893ee890e8633d9c3
Ray Villalobos <ray@planetoftheweb.com>
2017-08-18 18:19:41 -0400

    added files

---
+++ process/AppointmentList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <ul class="appointment-list"
+    v-for="(item, i) in appointments"
+    :key="item.id">
+    <li >{{item.petName}}</li>
+  </ul><!-- appointment-list --> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppointmentsList',
+  props: ['appointments'],
+} //export
+</script>
+
+<style scoped>
+
+  .appointment-list {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    margin: 10px 5%;
+    font-size: 1.7rem;
+  }
+  
+</style>
